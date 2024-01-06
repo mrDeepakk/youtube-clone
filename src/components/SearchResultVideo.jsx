7452938be11cecf3b59d811c1ba229d9413d7d76
Deepak kumar <deepak7589467031@gmail.com>
2024-01-06 09:07:53 +0530

    initial commit

---
+++ src/components/SearchResultVideo.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function SearchResultVideo() {
+  return <div>SearchResultVideo</div>;
+}
+
+export default SearchResultVideo;
---
+++ src/components/SuggestionVideo.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function SuggestionVideo() {
+  return <div>SuggestionVideo</div>;
+}
+
+export default SuggestionVideo;
